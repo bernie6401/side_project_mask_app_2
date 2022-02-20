89502f02f958028c89b05ebfc9507df0f88f7deb
bernie6401 <48666130+bernie6401@users.noreply.github.com>
2022-02-21 01:04:46 +0800

    Initial commit for version 2

---
+++ README.md
@@ -0,0 +1,10 @@
+# side_project_mask_app
+
+## Important FIle
+> app/build.gradle
+>
+> app/src/main/AndroidManifest.xml
+>
+> app/src/main/java/com/example/mask_app_android/MainActivity.kt
+>
+> app/src/main/res/layout/activity_main.xml
---
+++ app/src/main/java/com/example/mask_app_android_2/data/PharmacyInfo.kt
@@ -0,0 +1,37 @@
+package com.example.mask_app_android_2.data
+
+import com.google.gson.annotations.SerializedName
+
+data class PharmacyInfo(
+    val features: List<Feature>,
+    val type: String
+)
+
+data class Feature(
+    val geometry: Geometry,
+    val properties: Properties,
+    val type: String
+)
+
+data class Geometry(
+    val coordinates: List<Double>,
+    val type: String
+)
+
+data class Properties(
+    val address: String,
+    val available: String,
+    val county: String,
+    val cunli: String,
+    val custom_note: String,
+    val id: String,
+    val mask_adult: Int,
+    val mask_child: Int,
+    val name: String,
+    val note: String,
+    val phone: String,
+    val service_periods: String,
+    val town: String,
+    val updated: String,
+    val website: String
+)
